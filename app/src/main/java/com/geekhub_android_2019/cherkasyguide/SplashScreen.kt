02d25a90196b7fe8a94e299ebdf5cb48f5a035cb
Mariia <mashadia4@gmail.com>
2020-02-13 15:21:37 +0200

    add start screen, navigation drawer, splash screen

---
+++ app/src/main/java/com/geekhub_android_2019/cherkasyguide/SplashScreen.kt
@@ -0,0 +1,15 @@
+package com.geekhub_android_2019.cherkasyguide
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
