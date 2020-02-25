33286ab35a6f0b6c61556a46f975959a4f95270d
Mariia <mashadia4@gmail.com>
2020-02-25 21:04:24 +0200

    map fragment

--- app/src/main/java/com/geekhub_android_2019/cherkasyguide/ui/placemap/PlaceMapFragment.kt
+++ app/src/main/java/com/geekhub_android_2019/cherkasyguide/ui/placemap/PlaceMapFragment.kt
@@ -1,5 +1,53 @@
 package com.geekhub_android_2019.cherkasyguide.ui.placemap
 
+import android.content.Context
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.navArgs
+import com.geekhub_android_2019.cherkasyguide.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import kotlinx.android.synthetic.main.fragment_place_map.*
 
-class PlaceMapFragment : Fragment() {}
+class PlaceMapFragment : Fragment(R.layout.fragment_place_map), OnMapReadyCallback {
+
+    private val args: PlaceMapFragmentArgs by navArgs()
+    private val placeViewModel: PlaceViewModel by activityViewModels()
+    private lateinit var mMap: GoogleMap
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        map_view.onCreate(savedInstanceState)
+        map_view.onResume()
+
+        map_view.getMapAsync(this)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        placeViewModel.init(args.places)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        placeViewModel.addMarker(mMap)
+        placeViewModel.moveCamera(mMap)
+    }
+
+    override fun onResume() {
+        map_view.onResume()
+        super.onResume()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        map_view.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        map_view.onLowMemory()
+    }
+}
