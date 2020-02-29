6307406917df22316584740a9038ec2d91d8d39f
worker4food <worker4food@users.noreply.github.com>
2020-02-29 21:50:26 +0200

    Sign in in Splash activity

--- app/src/main/java/com/geekhub_android_2019/cherkasyguide/SplashScreen.kt
+++ app/src/main/java/com/geekhub_android_2019/cherkasyguide/SplashScreen.kt
@@ -3,13 +3,21 @@ package com.geekhub_android_2019.cherkasyguide
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+        lifecycleScope.launch {
+            FirebaseAuth.getInstance().signInAnonymously().await()
+
+            startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+            finish()
+        }
     }
 }
