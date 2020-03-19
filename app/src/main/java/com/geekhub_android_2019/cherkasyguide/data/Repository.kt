06eb469dd7fad7745ff1193a5a2f894ba773e47c
worker4food <worker4food@users.noreply.github.com>
2020-03-19 19:49:41 +0200

    Revert changes in Repository.kt

--- app/src/main/java/com/geekhub_android_2019/cherkasyguide/data/Repository.kt
+++ app/src/main/java/com/geekhub_android_2019/cherkasyguide/data/Repository.kt
@@ -1,11 +1,13 @@
 package com.geekhub_android_2019.cherkasyguide.data
 
-import com.geekhub_android_2019.cherkasyguide.models.Place
-import com.geekhub_android_2019.cherkasyguide.models.Route
+import com.geekhub_android_2019.cherkasyguide.models.*
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.tasks.await
 
 class Repository {
 
