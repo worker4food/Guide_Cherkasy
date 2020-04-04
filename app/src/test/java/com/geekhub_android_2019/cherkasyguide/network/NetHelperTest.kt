9a1bbb67b44a4cb0197bbcf4c772b8fcf5896559
Tetiana <tanyshechka16@gmail.com>
2020-04-04 17:40:57 +0300

    пробні тести

--- app/src/test/java/com/geekhub_android_2019/cherkasyguide/network/NetHelperTest.kt
+++ app/src/test/java/com/geekhub_android_2019/cherkasyguide/network/NetHelperTest.kt
@@ -1,12 +1,24 @@
+@file:Suppress("DEPRECATION")
+
 package com.geekhub_android_2019.cherkasyguide.network
 
 import android.app.Application
+import android.content.Context
 import android.net.ConnectivityManager
 import android.net.Network
+import android.net.NetworkInfo
+import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
+import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
 
+
+@Suppress("DEPRECATION")
+@RunWith(MockitoJUnitRunner.Silent::class)
 class NetHelperTest {
 
     private lateinit var application: Application
@@ -20,9 +32,38 @@ class NetHelperTest {
 
         network = mock()
 
-    }
+        val info:NetworkInfo = mock{
+            on{isConnected} doReturn true
+            on{type} doReturn (ConnectivityManager.TYPE_ETHERNET)
+            }
 
-        @Test
+        val connectivityManager:ConnectivityManager = mock{
+            on{activeNetworkInfo} doReturn info
+            on{}
+        }
+
+
+        }
+
+
+
+}
+
+/*@Test
+fun testNetworkStateMeteredNotRoaming() {
+    val networkInfo: NetworkInfo = mock(NetworkInfo::class.java)
+    `when`(networkInfo.isConnected).thenReturn(true)
+    `when`(networkInfo.isConnectedOrConnecting).thenReturn(true)
+    `when`(networkInfo.type).thenReturn(ConnectivityManager.TYPE_MOBILE)
+    `when`(networkInfo.isRoaming).thenReturn(false)
+    val connectivityManager: ConnectivityManager = mock(ConnectivityManager::class.java)
+    `when`(connectivityManager.activeNetworkInfo).thenReturn(networkInfo)
+    val context: Context = mock(MockContext::class.java)
+    `when`(context.getSystemService(Context.CONNECTIVITY_SERVICE))
+        .thenReturn(connectivityManager) }*/
+
+
+@Test
     fun isOnline() {
 
     }
@@ -38,6 +79,8 @@ class NetHelperTest {
     @Test
     fun isOverEthernet() {
     }
+
+
     
 
 }
---
+++ app/src/test/java/com/geekhub_android_2019/cherkasyguide/ui/placeslist/PlacesListViewModelTest.kt
@@ -0,0 +1,23 @@
+package com.geekhub_android_2019.cherkasyguide.ui.placeslist
+
+import com.geekhub_android_2019.cherkasyguide.data.Repository
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner.Silent::class)
+class PlacesListViewModelTest {
+
+    @Mock
+    private lateinit var repo: Repository
+
+    @Test
+    fun testsGetPlaces(repo: Repository) {
+
+
+    }
+}
+
