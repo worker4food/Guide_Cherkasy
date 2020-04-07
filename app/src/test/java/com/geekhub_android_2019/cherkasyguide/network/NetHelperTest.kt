10e54378cea872223047dd22f4b5da2377cbe777
Tetiana <tanyshechka16@gmail.com>
2020-04-07 20:39:04 +0300

    test on connection internet

--- app/src/test/java/com/geekhub_android_2019/cherkasyguide/network/NetHelperTest.kt
+++ app/src/test/java/com/geekhub_android_2019/cherkasyguide/network/NetHelperTest.kt
@@ -1,86 +1,89 @@
-@file:Suppress("DEPRECATION")
-
 package com.geekhub_android_2019.cherkasyguide.network
 
-import android.app.Application
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkCapabilities.*
 import android.net.NetworkInfo
-import com.nhaarman.mockitokotlin2.doReturn
+import android.os.Build
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.nhaarman.mockitokotlin2.mock
-import org.junit.Assert.assertThat
+import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
 
 
-/*@Suppress("DEPRECATION")
+@Suppress("DEPRECATION")
 @RunWith(MockitoJUnitRunner.Silent::class)
 class NetHelperTest {
-
-    private lateinit var application: Application
+    @Rule
+    @JvmField
+    val instantExecutorRule = InstantTaskExecutorRule()
     private lateinit var netHelper: NetHelper
-    private lateinit var connectivityManager: ConnectivityManager
-    private lateinit var networkCallback: ConnectivityManager.NetworkCallback
-    private lateinit var network: Network
 
     @Before
     fun setUp() {
+        val network: Network = mock()
 
-        network = mock()
+        val networkInfo: NetworkInfo = mock()
+        `when`(networkInfo.isConnected).thenReturn(true)
 
-        val info:NetworkInfo = mock{
-            on{isConnected} doReturn true
-            on{type} doReturn (ConnectivityManager.TYPE_ETHERNET)
-            }
+        val networkAvailability: NetworkCapabilities = mock()
+        `when`(networkAvailability.hasTransport(TRANSPORT_CELLULAR)).thenReturn(false)
+        `when`(networkAvailability.hasTransport(TRANSPORT_WIFI)).thenReturn(true)
+        `when`(networkAvailability.hasTransport(TRANSPORT_ETHERNET)).thenReturn(true)
 
-        val connectivityManager:ConnectivityManager = mock{
-            on{activeNetworkInfo} doReturn info
-            on{}
-        }
+        val connectivityManager: ConnectivityManager = mock()
+        `when`(connectivityManager.activeNetworkInfo)
+            .thenReturn(networkInfo)
+        `when`(connectivityManager.getNetworkCapabilities(network))
+            .thenReturn(networkAvailability)
 
+        val context: Context = mock()
+        `when`(context.getSystemService(Context.CONNECTIVITY_SERVICE))
+            .thenReturn(connectivityManager)
 
-        }
+        netHelper = NetHelper(context)
+    }
 
-
-
-}
-
-*//*@Test
-fun testNetworkStateMeteredNotRoaming() {
-    val networkInfo: NetworkInfo = mock(NetworkInfo::class.java)
-    `when`(networkInfo.isConnected).thenReturn(true)
-    `when`(networkInfo.isConnectedOrConnecting).thenReturn(true)
-    `when`(networkInfo.type).thenReturn(ConnectivityManager.TYPE_MOBILE)
-    `when`(networkInfo.isRoaming).thenReturn(false)
-    val connectivityManager: ConnectivityManager = mock(ConnectivityManager::class.java)
-    `when`(connectivityManager.activeNetworkInfo).thenReturn(networkInfo)
-    val context: Context = mock(MockContext::class.java)
-    `when`(context.getSystemService(Context.CONNECTIVITY_SERVICE))
-        .thenReturn(connectivityManager) }*//*
-
-
-@Test
+    @Test
     fun isOnline() {
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            assertEquals(netHelper.isOnline, false)
+        } else {
+            assertEquals(netHelper.isOnline, true)
+        }
     }
 
     @Test
     fun isOverWifi() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            assertEquals(netHelper.isOverWifi, true)
+        } else {
+            assertEquals(netHelper.isOverWifi, false)
+        }
     }
 
     @Test
     fun isOverCellular() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            assertEquals(netHelper.isOverCellular, true)
+        } else {
+            assertEquals(netHelper.isOverCellular, false)
+        }
     }
 
     @Test
     fun isOverEthernet() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            assertEquals(netHelper.isOverEthernet, true)
+        } else {
+            assertEquals(netHelper.isOverEthernet, false)
+        }
     }
-
-
-    
-
-}*/
+}
--- app/src/test/java/com/geekhub_android_2019/cherkasyguide/ui/placeslist/PlacesListViewModelTest.kt
+++ app/src/test/java/com/geekhub_android_2019/cherkasyguide/ui/placeslist/PlacesListViewModelTest.kt
@@ -1,54 +1,131 @@
 package com.geekhub_android_2019.cherkasyguide.ui.placeslist
 
-import androidx.lifecycle.LiveDataScope
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
+import androidx.lifecycle.Observer
 import com.geekhub_android_2019.cherkasyguide.data.Repository
 import com.geekhub_android_2019.cherkasyguide.models.Place
-import com.nhaarman.mockitokotlin2.mock
-import org.junit.Test
-
-import org.junit.Assert.*
+import junit.framework.Assert.assertNotNull
+import junit.framework.Assert.assertTrue
+import kotlinx.coroutines.flow.flowOf
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.ArgumentMatchers
 import org.mockito.Mock
-import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
-import java.util.*
-import androidx.lifecycle.LiveData
-import com.geekhub_android_2019.cherkasyguide.models.Places
-import com.nhaarman.mockitokotlin2.doReturn
-import kotlinx.coroutines.flow.Flow
-import org.junit.Rule
-import org.mockito.Mockito.`when`
 
 
 @RunWith(MockitoJUnitRunner.Silent::class)
 class PlacesListViewModelTest {
 
+    @Rule
+    @JvmField
+    val instantExecutorRule = InstantTaskExecutorRule()
+
     @Mock
     lateinit var repo: Repository
-    lateinit var fakePlace: Flow<List<Place>>
+    lateinit var placesListViewModel: PlacesListViewModel
 
+    @Mock
+    lateinit var lifecycleOwner: LifecycleOwner
+    var lifecycle: Lifecycle? = null
+    lateinit var observer: Observer<List<Place>>
 
     @Before
     fun setUp() {
-        repo = mock()
-        fakePlace = mock()
+        MockitoAnnotations.initMocks(this)
+        lifecycle = LifecycleRegistry(lifecycleOwner)
+        placesListViewModel = PlacesListViewModel(repo)
+        placesListViewModel.places.observeForever(observer)
     }
 
     @Test
-    fun fetchGetPlaces() {
-        // GIVEN
-        `when`(repo.getPlaces()).thenReturn(fakePlace)
-
-        // WHEN
-
-        // THEN
-        Mockito.verify(repo).getPlaces()
-
-
+    fun fetchPlacesNull() {
+        `when`(repo.getPlaces())
+            .thenReturn(null)
+        assertNotNull(placesListViewModel.places)
+        assertTrue(placesListViewModel.places.hasObservers())
     }
 
+    @Test
+    fun testFetchPlaces() {
+        `when`(repo.getPlaces())
+            .thenReturn(flowOf(List()))
+        placesListViewModel.places
+        verify(observer).onChanged(placesListViewModel.places.value)
+    }
+
+
+    /* @Mock
+     lateinit var repo: Repository
+     lateinit var placesListViewModel: PlacesListViewModel
+
+     //    lateinit var lifecycleOwner: LifecycleOwner
+ //    var lifecycle: Lifecycle? = null
+ //    private val observer: Observer<List<Place>> = mock()
+
+     @Before
+     fun setUp() {
+         repo = mock()
+         placesListViewModel = mock()
+         placesListViewModel = PlacesListViewModel(repo)
+ //        placesListViewModel.places.observeForever(observer)
+
+     }
+
+
+     @Test
+     fun moveToTopOfList() {
+         val placesListViewModel = PlacesListViewModel(Repository())
+         val lifecycle = LifecycleRegistry(mockk()).apply {
+             handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+         }
+
+         // given
+         val observe = mockk<(List<Place>) -> Unit>()
+         every { observe.invoke(Places()) } returns Unit
+         placesListViewModel.places.observe({ lifecycle }, observe)
+
+         // when
+         placesListViewModel.places
+
+         // then
+         verify { observe.invoke(Places()) }
+     }
+ */
+
+//@Test
+//fun testsGetPlaces(){
+//    val fakePlaces = Place()
+//    placesListViewModel.places
+//
+//    val captor = ArgumentCaptor.forClass(Places::class.java)
+//    captor.run {
+//        verify(observer).onChanged(capture())
+//        assertEquals(fakePlaces, value)
+//    }
+//}
+
+    /*  @Test
+      fun fetchPlacesNull() {
+          `when`(repo.getPlaces())
+              .thenReturn(null)
+          assertNotNull(placesListViewModel.places)
+          assertTrue(placesListViewModel.places.hasObservers())
+      }
+
+      @Test
+      fun testFetchPlaces() {
+          `when`(repo.getPlaces())
+              .thenReturn(List<Place>)
+          placesListViewModel.places
+          verify(observer).onChanged(placesListViewModel.places.value)
+      }*/
 }
 
