d9f285b4629f287024574a72f1e9c3067d505d8d
worker4food <worker4food@users.noreply.github.com>
2020-04-07 20:42:56 +0300

    Add separate koin FirebaseModule, fix koin test

--- app/src/main/java/com/geekhub_android_2019/cherkasyguide/di/FirebaseModule.kt
+++ app/src/main/java/com/geekhub_android_2019/cherkasyguide/di/FirebaseModule.kt
@@ -1,12 +1,13 @@
 package com.geekhub_android_2019.cherkasyguide.di
 
-import com.geekhub_android_2019.cherkasyguide.data.Repository
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.ktx.app
 import org.koin.dsl.module
 
-val repositoryModule = module {
-    single { Repository(get(), get()) }
+val firebaseModule = module {
+    single { Firebase.app }
+    single { Firebase.firestore(get()).document("/") }
+    single { FirebaseAuth.getInstance(get()) }
 }
--- app/src/test/java/com/geekhub_android_2019/cherkasyguide/koin/ModulesTest.kt
+++ app/src/test/java/com/geekhub_android_2019/cherkasyguide/koin/ModulesTest.kt
@@ -4,8 +4,14 @@ import android.content.Context
 import android.net.ConnectivityManager
 import androidx.core.content.getSystemService
 import com.geekhub_android_2019.cherkasyguide.appModules
+import com.geekhub_android_2019.cherkasyguide.data.Repository
+import com.geekhub_android_2019.cherkasyguide.di.firebaseModule
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.mockkStatic
 import org.junit.Test
 import org.junit.experimental.categories.Category
 import org.koin.dsl.module
@@ -16,16 +22,19 @@ import org.koin.test.check.checkModules
 @Category(CheckModuleTest::class)
 class ModulesTest : KoinTest {
 
-    private val ctxM = module {
+    private val unitTestModule = module(override = true) {
         single {
             mockk<Context> {
                 every { getSystemService<ConnectivityManager>() } returns mockk()
             }
         }
+
+        // Since Firebase is not mockable(?), just provide Repository
+        single { mockk<Repository>() }
     }
 
     @Test
     fun checkAllModules() = checkModules {
-        modules(ctxM + appModules)
+        modules(appModules + unitTestModule)
     }
 }
